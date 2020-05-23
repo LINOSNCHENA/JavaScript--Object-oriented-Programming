1eaa4fb3052039be164df597ee2964117f4bcfcf
Linos Nchena <linosnchena@gmail.com>
2020-05-23 11:37:50 +0200

    First grocery commit

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { products } from '../products';
 
 @Component({
   selector: 'app-product-list',
@@ -9,7 +10,12 @@ export class ProductListComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {  }
+  products = products;
+
+  share() {
+    window.alert('The product has been shared!');
   }
+
 
 }
